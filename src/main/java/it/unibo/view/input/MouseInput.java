605c57434d00dc679253e872ec2bf6e5c9ccb86a
Giulia Golesano <giulia.golesano@studio.unibo.it>
2024-03-23 20:03:13 +0100

    MouseInput class created, waiting for the gamestate enum

---
+++ src/main/java/it/unibo/view/input/MouseInput.java
@@ -0,0 +1,25 @@
+package it.unibo.view.input;
+
+import java.awt.event.*;;
+
+public class MouseInput implements KeyListener{
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
+    
+}
