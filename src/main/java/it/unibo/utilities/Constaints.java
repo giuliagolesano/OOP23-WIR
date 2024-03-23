913c05fe6851768708f5d728a3c4b129955fcccc
Giulia Golesano <giulia.golesano@studio.unibo.it>
2024-03-23 19:42:37 +0100

    movements constaints

---
+++ src/main/java/it/unibo/utilities/Constaints.java
@@ -0,0 +1,47 @@
+package it.unibo.utilities;
+
+public class Constaints {
+    
+    public static class Movements{
+        /**
+         * Pressed Key A.
+         */
+        public static final int LEFT = 65;
+        /**
+         * Pressed Left arrow.
+         */
+        public static final int LEFT_ARROW = 37;
+        /**
+         * Pressed Key D.
+         */
+        public static final int RIGHT = 68;
+        /**
+         * Pressed Right arrow.
+         */
+        public static final int RIGHT_ARROW = 39;
+        /**
+         * Pressed Key W.
+         */
+        public static final int UP = 87;
+        /**
+         * Pressed Up arrow.
+         */
+        public static final int UP_ARROW = 38;
+        /**
+         * Pressed Key S.
+         */
+        public static final int DOWN = 83;
+        /**
+         * Pressed Down arrow.
+         */
+        public static final int DOWN_ARROW = 40;
+        /**
+         * Pressed Key T
+         */
+        public static final int FIX = 84;
+        /**
+         * Pressed Esc.
+         */
+        public static final int ESCAPE = 27;
+    }
+}
