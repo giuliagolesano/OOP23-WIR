992baeea071e3fffdaf31c1e4d65a65e44d1f560
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2024-03-24 16:33:32 +0100

    created EntityImpl which implements Entity, currently only the position getter

--- src/main/java/it/unibo/model/api/Entity.java
+++ src/main/java/it/unibo/model/api/Entity.java
@@ -1,9 +1,17 @@
 package it.unibo.model.api;
 
+import it.unibo.common.Pair;
+
 /**
  * Entity
  */
 public interface Entity {
 
-    
+    /**
+     * Get the position of the entity.
+     *
+     * @return the position of the entity as a Pair of Doubles representing the x and y coordinates
+     */
+    Pair<Double, Double> getPosition();
+
 }
