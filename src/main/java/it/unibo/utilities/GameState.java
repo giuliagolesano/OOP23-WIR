1e5d1e4f55cb42910c91a78c29514dcb9dbc9dca
Giovanni Rinchiuso <giovanni.rinchiuso@studio.unibo.it>
2024-03-26 14:54:51 +0100

    implemented gamestate enum

--- src/main/java/it/unibo/utilities/GameState.java
+++ src/main/java/it/unibo/utilities/GameState.java
@@ -1,5 +1,23 @@
 package it.unibo.utilities;
 
 public enum GameState {
-    
+    HOME,
+
+    PLAYING,
+
+    PAUSED,
+
+    GAMEOVER,
+
+    SETTINGS;
+
+    private static GameState gamestate = HOME;
+
+    public static GameState getGameState() {
+        return gamestate;
+    }
+
+    public static void setGameState(final GameState state) {
+        gamestate = state;
+    }
 }
